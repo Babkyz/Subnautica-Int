de3e57c9cf8198ddf80896e233777107dfa5bee0
Babkyz <opasn@DESKTOP-01VIRU7>
2024-02-09 17:49:53 +0200

    Aboba

--- src/main/java/net/mcreator/namidessubnautica/init/NamidesSubnauticaModItems.java
+++ src/main/java/net/mcreator/namidessubnautica/init/NamidesSubnauticaModItems.java
@@ -12,12 +12,13 @@ import net.minecraft.world.level.block.Block;
 import net.minecraft.world.item.Item;
 import net.minecraft.world.item.BlockItem;
 
+import net.mcreator.namidessubnautica.block.display.OckerDisplayItem;
 import net.mcreator.namidessubnautica.NamidesSubnauticaMod;
 
 public class NamidesSubnauticaModItems {
 	public static final DeferredRegister<Item> REGISTRY = DeferredRegister.create(ForgeRegistries.ITEMS, NamidesSubnauticaMod.MODID);
 	public static final RegistryObject<Item> WALL_LOCKER = block(NamidesSubnauticaModBlocks.WALL_LOCKER);
-	public static final RegistryObject<Item> SUSUSUS = block(NamidesSubnauticaModBlocks.SUSUSUS);
+	public static final RegistryObject<Item> OCKER = REGISTRY.register(NamidesSubnauticaModBlocks.OCKER.getId().getPath(), () -> new OckerDisplayItem(NamidesSubnauticaModBlocks.OCKER.get(), new Item.Properties()));
 	public static final RegistryObject<Item> SD = block(NamidesSubnauticaModBlocks.SD);
 
 	private static RegistryObject<Item> block(RegistryObject<Block> block) {
