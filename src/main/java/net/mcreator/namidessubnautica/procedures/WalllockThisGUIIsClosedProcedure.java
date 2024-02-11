de366507e5c38b982818b69319b3ab6a07119916
Babkyz <opasn@DESKTOP-01VIRU7>
2024-02-11 20:16:48 +0200

    Криво работает

--- src/main/java/net/mcreator/namidessubnautica/procedures/WalllockThisGUIIsClosedProcedure.java
+++ src/main/java/net/mcreator/namidessubnautica/procedures/WalllockThisGUIIsClosedProcedure.java
@@ -5,14 +5,24 @@ import net.minecraft.world.level.block.state.BlockState;
 import net.minecraft.world.level.LevelAccessor;
 import net.minecraft.core.BlockPos;
 
+import net.mcreator.namidessubnautica.NamidesSubnauticaMod;
+
 public class WalllockThisGUIIsClosedProcedure {
 	public static void execute(LevelAccessor world, double x, double y, double z) {
 		{
-			int _value = 2;
 			BlockPos _pos = BlockPos.containing(x, y, z);
 			BlockState _bs = world.getBlockState(_pos);
-			if (_bs.getBlock().getStateDefinition().getProperty("animation") instanceof IntegerProperty _integerProp && _integerProp.getPossibleValues().contains(_value))
-				world.setBlock(_pos, _bs.setValue(_integerProp, _value), 3);
+			if (_bs.getBlock().getStateDefinition().getProperty("animation") instanceof IntegerProperty _integerProp)
+				world.setBlock(_pos, _bs.setValue(_integerProp, 0), 3);
 		}
+		NamidesSubnauticaMod.queueServerWork(1, () -> {
+			{
+				int _value = 2;
+				BlockPos _pos = BlockPos.containing(x, y, z);
+				BlockState _bs = world.getBlockState(_pos);
+				if (_bs.getBlock().getStateDefinition().getProperty("animation") instanceof IntegerProperty _integerProp && _integerProp.getPossibleValues().contains(_value))
+					world.setBlock(_pos, _bs.setValue(_integerProp, _value), 3);
+			}
+		});
 	}
 }
